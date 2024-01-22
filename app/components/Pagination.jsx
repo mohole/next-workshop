e440f9db377fcbb894711e57b321441e398e2a60
moebiusmania <moebiusmania@gmail.com>
2024-01-22 22:05:08 +0100

    fix: old files, navigation

--- app/components/Pagination.jsx
+++ app/components/Pagination.jsx
@@ -3,15 +3,15 @@ import styles from "./Pagination.module.css";
 
 export const Pagination = ({ next, prev }) => {
   const getPage = (url) => {
-    const number = url.split("?page=")[1];
-    return `/page/${number}`;
+    const number = url && url.split("?page=")[1];
+    return number ? `/page/${number}` : '#';
   };
 
   return (
     <section className={styles.pagination}>
-      <Link className={styles.link} href={prev ? getPage(prev) : undefined}>&lt; Prev</Link>
+      <Link className={styles.link} href={getPage(prev)}>&lt; Prev</Link>
       <span>---</span>
-      <Link className={styles.link} href={next ? getPage(next) : undefined}>Next &gt;</Link>
+      <Link className={styles.link} href={getPage(next)}>Next &gt;</Link>
     </section>
   );
 };
