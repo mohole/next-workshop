12dd70ec37adf2a2129ca6c1922531ba83faf5bf
moebiusmania <moebiusmania@gmail.com>
2024-02-11 11:23:20 +0100

    chore: resetting the project to an initial state

--- app/page.js
+++ app/page.js
@@ -1,30 +1,7 @@
-import { sql } from "@vercel/postgres";
-import { Todos } from "./components/Todos";
-
-const getData = async () => {
-  /**
-   * The "classic" way to query the database in Next.js 12 and below
-   */
-  /*
-  const res = await fetch("/api/todo");
-  const data = await res.json();
-  return data;
-  */
-
-  /**
-   * In Next.js 13+ server components, you can query the database directly
-   */
-  const { rows } = await sql`SELECT * FROM TodoList`;
-  return rows;
-};
-
 export default async function Home() {
-  const data = await getData();
-
   return (
     <main>
       <h1>todo app</h1>
-      <Todos data={data} />
     </main>
   );
 }
